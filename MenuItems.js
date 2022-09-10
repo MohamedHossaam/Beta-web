db7e3aef5296473ec50201360d05d735cd0d64d2
Mohamed Hossam <110938643+MohamedHossaam@users.noreply.github.com>
2022-09-10 14:59:46 +0200

    Add files via upload

---
+++ MenuItems.js
@@ -0,0 +1,26 @@
+const menuItems = [
+    {
+      title: "Home",
+      url: "#",
+      cName: "nav-links"
+    },
+  
+    {
+      title: "About",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Courses",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Sign up",
+      url: "#",
+      cName: "nav-links-mobile"
+    }
+  ];
+  
+  export default menuItems;
+  
